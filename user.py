787820ee4c05460ad55e9c96fb8c758ee3e3a051
HKavalos <hazelavalos@yahoo.com>
2021-06-17 16:24:12 -0400

    Added some new params for event and user

--- user.py
+++ user.py
@@ -1,9 +1,17 @@
 class User:
     """A class to store data about each user"""
 
-    def __init__(self, name):
+    def __init__(self, name, groups):
         self._name = name
+        self._groups = groups
 
     @property
     def name(self):
         return self._name
+    @property
+    def groups(self):
+        return self._groups
+
+    @groups.setter
+    def time(self, groups):
+        self._groups = groups
