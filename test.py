0749d870d1a94bb37aa8725b9a1bba7554c718e8
HKavalos <hazelavalos@yahoo.com>
2021-06-27 18:13:51 -0400

    Yay things are working

--- test.py
+++ test.py
@@ -562,8 +562,8 @@ class Ui_MainWindow(QMainWindow):  # changed to QMainWindow from object
 
     def update_event(self, event):
         self.event_title.setText(event.description)
-        self.event_date.setText(event.time)  # time equals place??
-        self.event_place.setText(event.place)
+        self.event_date.setText(event.options)  # time equals place??
+        self.event_place.setText(event.status)
 
 
 class LogIn(QMainWindow):
